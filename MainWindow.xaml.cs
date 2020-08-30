3b415c9e3d97138f9d5bd726f623e741d790a72c
Roman <tcheremnov@yandex.ru>
2020-08-30 22:06:02 +0300

    First repo loading to git

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Threading;
+
+namespace PrintScreenMaker
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ReadConfig();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+          
+            
+        }
+
+        private void txtBoxFileDir_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            FolderBrowserDialog flDlg = new FolderBrowserDialog();
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            flDlg.ShowDialog();
+
+            txtBoxFilePath.Text = flDlg.SelectedPath;
+            
+        }
+
+        private void ReadConfig()
+        {
+            txtBoxInterval.Text = ConfigurationManager.AppSettings.Get("Key0");
+            txtBoxFilePath.Text = ConfigurationManager.AppSettings.Get("Key1");
+            Key1 = txtBoxFilePath.Text;
+            Key0 = Convert.ToInt32(txtBoxInterval.Text);
+        }
+
+        
+        private void SetConfig(int Key0, string Key1)
+        {
+            ConfigurationManager.AppSettings.Set("Key0", Key0.ToString());
+            ConfigurationManager.AppSettings.Set("Key1", Key1.ToString());
+        }
+
+        int Key0;
+        string Key1;
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Key1 = txtBoxFilePath.Text;
+            Key0 = Convert.ToInt32(txtBoxInterval.Text);
+            SetConfig(Key0, Key1);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            
+            timerStart();
+        }
+
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+       
+        private void timerStart()
+        {
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, Key0);
+            dispatcherTimer.Start();
+        }
+
+        MakeScreenShot mkScrnShoot = new MakeScreenShot();
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            mkScrnShoot.CaptureMyScreen(Key1);
+        }
+    }
+}
---
+++ MakeScreenShot.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace PrintScreenMaker
+{
+    class MakeScreenShot
+    {
+
+        public void CaptureMyScreen(string path)
+
+        {
+
+            try
+
+            {
+
+                //Creating a new Bitmap object
+
+                Bitmap captureBitmap = new Bitmap(1440, 900, PixelFormat.Format64bppArgb);
+
+
+                //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
+
+                //Creating a Rectangle object which will  
+
+                //capture our Current Screen
+
+                Rectangle captureRectangle = Screen.PrimaryScreen.WorkingArea;
+
+
+
+                //Creating a New Graphics Object
+
+                Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+
+
+
+                //Copying Image from The Screen
+
+                captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+
+
+                Font font = new Font("Arial", 25, FontStyle.Bold, GraphicsUnit.Pixel);
+                Color color = Color.FromArgb(230, 186, 186, 1);
+                Point atpoint = new Point(captureBitmap.Width-180, captureBitmap.Height-50);
+                SolidBrush brush = new SolidBrush(color);
+              //  Graphics graphics = Graphics.FromImage(captureBitmap);
+                StringFormat sf = new StringFormat();
+                sf.Alignment = StringAlignment.Center;
+                sf.LineAlignment = StringAlignment.Center;
+                captureGraphics.DrawString("CheremonGames.com", font, brush, atpoint, sf);
+                captureGraphics.Dispose();
+
+                //Saving the Image File (I am here Saving it in My E drive).
+                string imgFilename = "Image_" + DateTime.Now.ToString("hhMMss");
+                captureBitmap.Save(@""+path+"\\"+imgFilename+".jpg", ImageFormat.Jpeg);
+                captureBitmap.Dispose();
+
+
+
+                //Displaying the Successfull Result
+
+
+
+                //  MessageBox.Show("Screen Captured");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+    }
+}
